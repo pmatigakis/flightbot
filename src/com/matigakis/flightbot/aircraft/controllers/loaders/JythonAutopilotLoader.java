abaf391910a5bbeba471430ea99cb90fa352ebdd
Panagiotis Matigakis <pmatigakis@gmail.com>
2014-06-12 15:47:58 +0300

    Now you have to give the path to the autopilot when calling the start_flightbot.sh script.

--- src/com/matigakis/flightbot/aircraft/controllers/loaders/JythonAutopilotLoader.java
+++ src/com/matigakis/flightbot/aircraft/controllers/loaders/JythonAutopilotLoader.java
@@ -11,11 +11,11 @@ import com.matigakis.flightbot.aircraft.controllers.AircraftController;
 public class JythonAutopilotLoader implements AutopilotLoader {
 	public AircraftController getAutopilot(String autopilotName){
 		PySystemState sys = Py.getSystemState();
-		sys.path.append(new PyString("./autopilots"));
+		sys.path.append(new PyString(autopilotName));
 		
 		PythonInterpreter interpreter = new PythonInterpreter(null, sys);
 		
-		interpreter.exec("from " + autopilotName + ".autopilot import Autopilot");
+		interpreter.exec("from autopilot import Autopilot");
 		
 		PyObject controllerClass = interpreter.get("Autopilot");
 		

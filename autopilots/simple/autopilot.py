cc4f213acfda84fa7cd6e49e28903f269ce652cf
Panagiotis Matigakis <pmatigakis@gmail.com>
2014-11-19 14:32:46 +0200

    Moved the PID controller class to a separate library.

--- autopilots/simple/autopilot.py
+++ autopilots/simple/autopilot.py
@@ -2,32 +2,17 @@ from java.lang import Math
 
 from com.matigakis.flightbot.aircraft.controllers import Autopilot
 from com.matigakis.flightbot.navigation import Navigator
-from com.matigakis.flightbot.control import PID
+from com.matigakis.controlsystems.pid import PID
 
-
-#from pid import PID
-
-#def distance(lat1, lon1, lat2, lon2):
-#    phi1 = Math.toRadians(lat1)
-#    phi2 = Math.toRadians(lat2)
-#    lam1 = Math.toRadians(lon1)
-#    lam2 = Math.toRadians(lon2)
-
-#    return 6371.01 * Math.acos( Math.sin(phi1) * Math.sin(phi2) + Math.cos(phi1) * Math.cos(phi2) * Math.cos(lam2 - lam1) );
-
-#def bearing(lat1, lon1, lat2, lon2):
-#    brng = Math.atan2(Math.sin(lon2-lon1)*Math.cos(lat2),
-#                      Math.cos(lat1)*Math.sin(lat2)-Math.sin(lat1)*Math.cos(lat2)*Math.cos(lon2-lon1))
-
-#    return Math.toDegrees(brng % (2.0 * 3.1415))
 
 class JythonAutopilot(Autopilot):
     def __init__(self):
-        self.throttle_pid = PID(0.1, 0.07, 0.05, 0.1, 10.0)
-        self.course_pid = PID(1.0, 0.0, 0.0, 0.1, 0.0)
-        self.aileron_pid = PID(0.1, 0.005, 0.001, 0.1, 10.0)
-        self.pitch_pid = PID(0.5, 0.0, 0.0, 0.1, 0.0)
-        self.elevator_pid = PID(0.055, 0.0005, 0.0001, 0.1, 200.0)
+        p = PID
+        self.throttle_pid = PID(0.1, 0.07, 0.05, 0.0, 1.0, 0.1, 10.0)
+        self.course_pid = PID(1.0, 0.0, 0.0, -15.0, 15.0, 0.1, 0.0)
+        self.aileron_pid = PID(0.1, 0.005, 0.001, -1.0, 1.0, 0.1, 10.0)
+        self.pitch_pid = PID(0.5, 0.0, 0.0, -15.0, 15.0, 0.1, 0.0)
+        self.elevator_pid = PID(0.055, 0.0005, 0.0001, -1.0, 1.0, 0.1, 200.0)
 
         self.navigator = Navigator()
 
@@ -58,12 +43,7 @@ class JythonAutopilot(Autopilot):
 
         e = self.target_airspeed - current_airspeed
 
-        target_throttle = self.throttle_pid.update(e)
-        
-        if target_throttle > 1.0:
-            target_throttle = 1.0
-        elif target_throttle < 0.0:
-            target_throttle = 0.0
+        target_throttle = self.throttle_pid.calculateOutput(e)
 
         controls.setThrottle(target_throttle)            
 
@@ -94,23 +74,13 @@ class JythonAutopilot(Autopilot):
         elif course_e > 180.0:
             course_e =  course_e - 360
 
-        target_roll = self.course_pid.update(course_e)
-
-        if target_roll > 15.0:
-            target_roll = 15.0
-        elif target_roll < -15.0:
-            target_roll = -15.0
+        target_roll = self.course_pid.calculateOutput(course_e)
 
         current_roll = instrumentation.getRoll()
 
         roll_e = target_roll - current_roll
 
-        target_aileron = self.aileron_pid.update(roll_e)
-
-        if target_aileron > 1.0:
-            target_aileron = 1.0
-        elif target_aileron < -1.0:
-            target_aileron = -1.0
+        target_aileron = self.aileron_pid.calculateOutput(roll_e)
 
         controls.setAileron(target_aileron)
 
@@ -118,22 +88,12 @@ class JythonAutopilot(Autopilot):
         current_altitude = instrumentation.getAltitude()
 
         altitude_e = self.target_altitude - current_altitude
-        target_pitch = self.pitch_pid.update(altitude_e)
-
-        if target_pitch > 15.0:
-            target_pitch = 15.0
-        elif target_pitch < -15.0:
-            target_pitch = -15.0
+        target_pitch = self.pitch_pid.calculateOutput(altitude_e)
 
         current_pitch = instrumentation.getPitch()
 
         pitch_e = target_pitch - current_pitch
-        target_elevator = -self.elevator_pid.update(pitch_e)
-
-        if target_elevator > 1.0:
-            target_elevator = 1.0
-        elif target_elevator < -1.0:
-            target_elevator = -1.0
+        target_elevator = -self.elevator_pid.calculateOutput(pitch_e)
 
         controls.setElevator(target_elevator)
 
